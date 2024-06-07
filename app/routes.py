b9051230eb3bd77cee4f41bbd0a2960d621ed0a6
Chukwukwadorom <chikwadonwattah@gmail.com>
2024-06-07 22:50:38 +0100

    the one where i added forms and config file

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import app
-from flask import render_template
+from flask import render_template, redirect, flash, url_for
+from app.forms import  LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -19,4 +20,14 @@ def index():
     user = {"username":"Chikwado"}
 
     return render_template("index.html", posts=posts, title="Home", user=user) 
-   
+
+
+@app.route('/login', methods=["POST","GET"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f"logging in {form.username.data}, remember me: {form.remember_me.data}")
+        return redirect(url_for("index"))
+
+    return render_template("login.html", form=form, title="Log In")
+    
---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+     SECRET_KEY = os.environ.get("SECRET_KEY")
+
+
+con = Config()
