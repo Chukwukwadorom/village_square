54d4bd3aaa06302a8193755712be2fbcbb48c511
Chukwukwadorom <chikwadonwattah@gmail.com>
2024-06-08 01:45:31 +0100

    the one where i update config.py with database config

--- config.py
+++ config.py
@@ -2,9 +2,10 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
+base_dir = os.path.abspath(os.path.dirname(__file__))
 class Config:
-     SECRET_KEY = os.environ.get("SECRET_KEY")
+     SECRET_KEY = os.environ.get("SECRET_KEY") or "attention is all you need"
+     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI") or f"sqlite:///{os.path.join(base_dir,'app.db')}"
 
 
 con = Config()
